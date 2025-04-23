ab2bc537daf601ee98561b8b41da09fbee9f30b4
Thalison Aragão <thalissonaragao01@gmail.com>
2025-04-23 10:11:26 -0300

    Styled Components

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello Word</h1>
+   <>
+     <Button variant= "primary"/>
+     <Button variant= "secondary"/>
+     <Button variant= "success"/>
+     <Button variant="danger"/>
+     <Button/>
+   </>
   )
 }
 
