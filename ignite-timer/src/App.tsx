b82bee939aa91565b1f4af61a9d693650d4ab067
Thalison Aragão <thalissonaragao01@gmail.com>
2025-04-25 21:00:07 -0300

    Configurando temas

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './components/styles/themes/default'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
+   <ThemeProvider theme={defaultTheme}>
      <Button variant= "primary"/>
      <Button variant= "secondary"/>
      <Button variant= "success"/>
      <Button variant="danger"/>
      <Button/>
-   </>
+   </ThemeProvider>
   )
 }
 
--- ignite-timer/src/components/Button.styles.ts
+++ ignite-timer/src/components/Button.styles.ts
@@ -16,9 +16,15 @@ const buttonVariants ={
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary};
+    
+
+    /* ${props => {
         return css` //Executa como função
             background-color: ${buttonVariants[props.variant]}
         ` 
-    }}`
+    }} */`
