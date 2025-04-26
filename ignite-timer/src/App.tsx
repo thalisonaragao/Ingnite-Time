e71bedfc44b83c9d35cea98c3a02a1fc57b38160
Thalison Aragão <thalissonaragao01@gmail.com>
2025-04-26 14:29:53 -0300

    Estilos globais

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
-import { defaultTheme } from './components/styles/themes/default'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 
 export function App() {
   return (
@@ -10,6 +12,7 @@ export function App() {
      <Button variant= "success"/>
      <Button variant="danger"/>
      <Button/>
+     <GlobalStyle />
    </ThemeProvider>
   )
 }
