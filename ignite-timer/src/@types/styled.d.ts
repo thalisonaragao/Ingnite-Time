7c73b8e3b7f186f24cadeabf0052a778b3157486
Thalison Aragão <thalissonaragao01@gmail.com>
2025-05-15 14:13:57 -0300

    Layout de rotas

--- ignite-timer/src/@types/styled.d.ts
+++ ignite-timer/src/@types/styled.d.ts
@@ -1,6 +1,8 @@
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
+// Esse arquivo serve para sobrescrever tipagem do stylede-components
+
 type ThemeType = typeof defaultTheme // Guarda as propriedades do tema
 
 declare module 'styled-components' {
