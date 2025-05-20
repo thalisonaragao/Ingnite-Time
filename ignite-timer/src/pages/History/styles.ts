27f346a54d88aff09b0e3ab8f1b4d23f47a59beb
Thalison Aragão <thalissonaragao01@gmail.com>
2025-05-20 10:21:12 -0300

    Componente: Status

--- ignite-timer/src/pages/History/styles.ts
+++ ignite-timer/src/pages/History/styles.ts
@@ -60,3 +60,26 @@ export const HistoryList = styled.div`
     }
   }
 `
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const // Serve para dizer type não ler como String mutavel, mas ler como imutavel. Resumo tá dizendo que só vai ser essas 3 cores que tá mo default.ts
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
