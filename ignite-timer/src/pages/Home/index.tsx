cc2ade9361c36ec76b8f2585ea5a531c3fa60f7a
Thalison Aragão <thalissonaragao01@gmail.com>
2025-05-29 18:58:57 -0300

    Funcionalidades da aplicação - Iniciando novo ciclo

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from 'react'
 
 import {
   CountdownContainer,
@@ -12,7 +13,6 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -22,14 +22,18 @@ const newCycleFormValidationSchema = zod.object({
     .max(90, 'O ciclo precisa ser no máximo 90 minutos'),
 })
 
-/* interface NewCycleFormData { // Pode usar esse para resolver o problema de tipo, pois typeScript não entende JS
-  task: string
-  minutesAmount: number
-} */
-
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> // Criando um tipo typescript
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -39,9 +43,21 @@ export function Home() {
   })
 
   function handlerCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle]) // Usamos desse jeito sempre que uma alteração de estado depender do valor anterior
+    setActiveCycleId(id)
     reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDisabled = !task
